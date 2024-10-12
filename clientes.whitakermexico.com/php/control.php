87d79f6430ca71d0579d63292178101aff31bbea
wtksistemas <sistemas@whitakermexico.com>
2024-10-11 19:04:14 -0600

    Me salio el error ( ! ) Notice: session_start(): Ignoring session_start() because a session is already active (started from C:\Users\sistemas 02\Documents\Git\WTK\clientes.whitakermexico.com\1\menu.php on line 12) in C:\Users\sistemas 02\Documents\Git\WTK\clientes.whitakermexico.com\php\control.php on line 2
    #	Time	Memory	Function	Location
    1	0.0005	443040	{main}( )	...\menu.php:0
    2	0.0008	444440	include( 'C:\Users\sistemas 02\Documents\Git\WTK\clientes.whitakermexico.com\php\control.php )	...\menu.php:15
    3	0.0008	444440	session_start( )	...\control.php:2
    
    Cambie a otra condicional y comente el que antes teniamos ln 24-35

--- clientes.whitakermexico.com/php/control.php
+++ clientes.whitakermexico.com/php/control.php
@@ -1,5 +1,27 @@
 <?php
-  session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start(); // Inicia la sesión solo si no está iniciada
+}
+
+if (!(isset($_SESSION["login"]) && $_SESSION["login"] == "OK")) {
+    // Si no hay sesión o el login no es OK, puedes redirigir o mostrar un mensaje
+} else {
+    header("Location: ../index.html");
+    exit(); // Asegúrate de detener la ejecución después de redirigir
+}
+
+
+//session_status() === PHP_SESSION_NONE Condicional asegura que session_start() solo se llame 
+//si no se ha iniciado ya una sesión.   
+?>
+
+
+
+
+
+
+<?php
+  /*session_start();
 
   if(!(isset($_SESSION["login"]) && $_SESSION["login"] == "OK"))
  {
@@ -10,6 +32,6 @@ else
 	
 header("Location: https://clientes.whitakermexico.com");
 
-}
+}*/
 
 ?>
