fad8c6ab4e8f37a46ed0490ecbb519ef896a88c1
wtksistemas <sistemas@whitakermexico.com>
2024-10-15 18:33:18 -0600

    documentacion de las funciones

--- clientes.whitakermexico.com/php/funciones.php
+++ clientes.whitakermexico.com/php/funciones.php
@@ -2,7 +2,7 @@
 /* Generacion de token para paso de url */
 function token(){
     $r1 = bin2hex(random_bytes(10));
-    
+
     $token = $r1;
 
     return $token;
@@ -39,12 +39,10 @@ function verifica_usuario($persona) // funcion para determinar si el usuario que
 
 	// Si el numero de filas obtenidas de la consulta es mayor a 0 guardamos el id y usuario obtenido 
 	if($num_rows>0)
-	{
-	$id_obtenido=$row["ID"];
-	$user_msql=$row["c_usuario"];
-
-	
-	}
+		{
+			$id_obtenido=$row["ID"];
+			$user_msql=$row["c_usuario"];
+		}
 
 	// Si la consulta no arroja ningun resultado..
  	if($row == false) 
@@ -53,28 +51,26 @@ function verifica_usuario($persona) // funcion para determinar si el usuario que
 		}
 	if($num_rows>0 && $persona==$user_msql) // si el numero de filas recibidas por la consulta es mayor a 0 y el usuario es igual al solicitante
 		{
-
 			// Generamos token
-				$t1=token();
-				$t2=token_2();
+			$t1=token();
+			$t2=token_2();
 
-
-				// secccion de inserciones 
-				$query="insert into tb_token (ID,id_cusuario,c_token,c_tipotoken,c_estado) values('".$espacio."','".$id_obtenido."','".$t1."','".$tipo_token1."','".$estado_tkn."')";
-				$query2="insert into tb_token (ID,id_cusuario,c_token,c_tipotoken,c_estado) values('".$espacio."','".$id_obtenido."','".$t2."','".$tipo_token2."','".$estado_tkn."')";
-
+			// secccion de inserciones 
+			$query="insert into tb_token (ID,id_cusuario,c_token,c_tipotoken,c_estado) values('".$espacio."','".$id_obtenido."','".$t1."','".$tipo_token1."','".$estado_tkn."')";
+			$query2="insert into tb_token (ID,id_cusuario,c_token,c_tipotoken,c_estado) values('".$espacio."','".$id_obtenido."','".$t2."','".$tipo_token2."','".$estado_tkn."')";
 
 			// insertamos tokens
 			$result_t1=mysqli_query($conn,$query);
 			$result_t2=mysqli_query($conn,$query2);
 			$r9=1;
 		}
-	else
+	else	//si no se encuentra el usuario asigna mensaje de error
 		{
-			$r9="Usuario no existente";
+			$r9="Usuario no existente"; 
 		}	
-	mysqli_close($conn);
-return [$t1,$t2];
+	
+	mysqli_close($conn);	//cierra la conexion de bd 
+	return [$t1,$t2];	//retorna los 2 tokens generados
 }
 
 
@@ -87,74 +83,77 @@ function login_user($user1,$password1) // Funcion para inicio de sesion
 
 	// Variables de validacion
 	$w=$user1;
-	// Seccion de consutlas
+	// Seccion de consultas
 	$sql = "select c_usuario,cast(aes_decrypt(c_password,'".$llave."')as char) from tb_usuarios where c_usuario='".$user1."';";
 
 	
 	$result = mysqli_query($conn,$sql);
-	$row = mysqli_fetch_array($result, MYSQLI_NUM);
-
-
-
-if($row == false)
-{
-
-	header("Location: ../index.html?v=9"); 
-
-}
-
-$user_msql=$row[0]; //Guardamos usuario obtenido por consulta
-$pass_msql=$row[1]; //Guardamos contraseña obtenida por consulta
-
-
-$num_rows = mysqli_num_rows($result);
-
-
-echo $num_rows."\n";
-if($user1<>'' || $pass_msql <>'')
-{
-
-	if($user_msql==$user1)
+	$row = mysqli_fetch_array($result, MYSQLI_NUM);// almacena la consulta en un arreglo
+	
+	if($row == false)//si no encuentra al usuario te regresa al incio 
 		{
-			if($pass_msql==$password1)
+			
+			header("Location: ../index.html?v=9");
+
+		}
+
+
+	$user_msql=$row[0]; //Guardamos usuario obtenido por consulta
+	$pass_msql=$row[1]; //Guardamos contraseña obtenida por consulta
+
+
+	$num_rows = mysqli_num_rows($result); //Comprobacion de la fila del usuario
+	echo $num_rows."\n";
+
+
+	if($user1<>'' || $pass_msql <>'')//si el usuario ingresado esta vacio o la contraseña vacio 
+		{
+			if($user_msql==$user1) //si el usuario ingresado es identico a la bd 
 				{
-					session_start();
-					$_SESSION['id']='888';
-					/* header("Location: https://clientes.whitakermexico.com/1/menu.php"); */
-				header("Location: ../1/menu.php");
+					if($pass_msql==$password1) //si la contraña ingresada es identica a la bd
+						{
+							session_start();	//inicia sesion
+							$_SESSION['id']='888';	//asigna un valor de id a la sesion
+							header("Location: ../1/menu.php");	//envia al menú
+						}
+					else	//si es contraseña es incorrcto envia al index valor 3
+						{
+	    					/*    session_destroy(); */
+							header("Location: ../index.html?v=3"); 
+						}
 				}
-			else
+			else	//si el usuario es incorrecto envia al index con valor 9
 				{
-	    			/*    session_destroy(); */
-					header("Location: ../index.html?v=3"); 
+					header("Location: ../index.html?v=9");  
 				}
 		}
-		else
-			{
-				header("Location: ../index.html?v=9"); 
-			}
-}
 }
 
-function com_tkn($tk1)
+
+function com_tkn($tk1)	//funcion para comprobar token ingresado para cambiar contraseña
 {	
-	require(__DIR__ . '/dbconnect.php');
+	require(__DIR__ . '/dbconnect.php');	//carga al archivo para conectarse a la bd
 
-	$z=null;
+	$z=null;	//Definicion de variables de validacion
 
+	// Seccion de consultas 
 	$sql = "select c_token, c_estado from tb_token where c_token='".$tk1."'and c_estado = 'activo' ;";
-	// buscamos al usuario que solicita la accion 
+	// buscamos el token de 4 digitos y que este activo 
 	$result = mysqli_query($conn,$sql);
 	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-	$num_rows = mysqli_num_rows($result);
+	$num_rows = mysqli_num_rows($result); //numero de fila donde el token existe y esta en  activo
 
-	if ($num_rows>0){
-		header("Location: ../confirmacion.html");
-		return $z;
-	}else{
-		header("Location: ../generacion.php?v=10");
-		return $z;
-	}
+	//Si en la consulta hay una fila es valido para cambiar contraseña
+	if ($num_rows>0)
+		{
+			header("Location: ../confirmacion.html");
+			return $z;
+		}
+	else
+		{ 		//si en la consulta no hay fila te regresa para volver ingresar el token
+			header("Location: ../generacion.php?v=10");
+			return $z;
+		}
 }
 
 
