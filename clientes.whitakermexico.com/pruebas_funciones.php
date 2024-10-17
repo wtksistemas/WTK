58614c3370f399f3372d7ac3c057716190d380dc
wtksistemas <sistemas@whitakermexico.com>
2024-10-17 14:45:13 -0600

    limpieza de archivo

--- clientes.whitakermexico.com/pruebas_funciones.php
+++ clientes.whitakermexico.com/pruebas_funciones.php
@@ -1,35 +1,4 @@
 <?php
 
-//$tk=$_POST['token'];
-function com_tkn($tk1)
-{	
-	require_once('php/dbconnect.php');
-	require(__DIR__ . '/php/funciones.php');
 
-	$z=null;
-
-
-list($a,$b)=verifica_usuario('sistemas@whitakermexico.com');
-
-	$sql = "select c_token, c_estado from tb_token where c_token='".$tk1."'and c_estado = 'activo' ;";
-	// buscamos al usuario que solicita la accion 
-	$result = mysqli_query($conn,$sql);
-	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-	$num_rows = mysqli_num_rows($result);
-
-	if ($num_rows>0){
-		$z="verdadero";
-		return $z;
-	}else{
-		$z="falso";
-		return $z;
-	}
-}
-
-
-
-
-
-$y=com_tkn(1);
-echo $y;
 ?>
