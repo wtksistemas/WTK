5db03ddaa36b08a2c5dece61ed30fdf5dfbcd735
wtksistemas <sistemas@whitakermexico.com>
2024-10-22 18:10:09 -0600

    quitar del menu principal, el calendario el menu lateral, y color de las letras de usuario

--- adm.whitakermexico.com/1/menu.php
+++ adm.whitakermexico.com/1/menu.php
@@ -6,6 +6,7 @@
     <title>Panel Principal</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
 	
 	<?php
@@ -39,20 +40,7 @@
 
 	
 	
-    <!-- Menú lateral izquierdo -->
-    <div class="menu-lateral">
-        <a href="menu.php"><img src="img/logo.svg" alt="Imagen superior"></a>
-	
-		
-		
-		<!--AGREGAR COMO LISTA-->
-		<ul class="nav">
-			<li><a href="#">Eventos</a></li>
-            <li><a href="#">Facturas</a></li>
-            <li><a href="#">Extra</a></li>
-		</ul>
-
-    </div>
+   
 	
     <!-- Contenedor principal -->
     <div class="contenedor-principal">
@@ -61,66 +49,21 @@
 	<div class="menu-superior">
 		<div class="opciones">
 			<a href="nomina.php">Nómina</a>
-			<a href="legal.php">Legal</a>
-			<a href="rh.php">RH</a>
 		</div>
         
 		<div class="perfil">
-			<button>Perfil</button>
-			
-	<?php
-	echo"".$_SESSION['username']."" ;
-	?></P> 
 
-	<a href="../php/logout.php"><button type="button">Cerrar sesion</button></a>
+			<?php
+				echo "<span style='color: white;'>".$_SESSION['username']."</span>";
+			?>
+			<a href="../php/logout.php"><button type="button">Cerrar sesión</button></a>
+
 		</div>
 	</div>
 	
 	<!-- Buscardor -->
         <!-- Contenido -->
-        <div class="contenido">
-					
-			
-			<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FMexico_City&bgcolor=%23EF6C00&title=MI%20CALENDARIO&src=c2lzdGVtYXNAd2hpdGFrZXJtZXhpY28uY29t&color=%23039BE5" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-
-            <!-- Barra de búsqueda 
-            <div class="barra-busqueda">
-				<label for="f_busqueda">Filtros de busqueda:</label>
-				
-				<select name="f_busqueda" id="f_busqueda">
-					<option value="cliente">Cliente</option>
-					<option value="n_escritura">N° de  Escritura</option>
-					<option value="fecha">Fecha</option>
-					<option value="nombre">Nombre</option>
-				</select>
-				
-				<input type="text" placeholder="Informacion">				
-            </div>
- -->
-            <!-- Lista de expedientes 
-            <div class="expediente">
-                <span>Expediente 1</span>
-                <div>
-                    <button>Ver</button>
-                    <button>Reservar</button>
-                    <button>Histórico</button>
-                    <button>Expediente Virtual</button>
-                </div>
-            </div>
-
-            <div class="expediente">
-                <span>Expediente 2</span>
-                <div>
-                    <button>Ver</button>
-                    <button>Reservar</button>
-                    <button>Histórico</button>
-                    <button>Expediente Virtual</button>
-                </div>
-            </div>-->
-
-            <!-- Puedes agregar más expedientes aquí -->
-
-        </div>
+       
 
     </div>
 </body>
