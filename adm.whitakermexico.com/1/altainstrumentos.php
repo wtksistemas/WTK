9746489823f5e8e3666eefb9f9ff4f65a10300a3
wtksistemas <sistemas@whitakermexico.com>
2024-11-21 14:50:02 -0600

    pruebas de nuevo cliente js

--- adm.whitakermexico.com/1/altainstrumentos.php
+++ adm.whitakermexico.com/1/altainstrumentos.php
@@ -35,55 +35,51 @@
 
         <div class="contenido formulario-contenido">
             <div class="formulario-nuevo-instrumento">
-                <form>
-                    <script src="../../clases_prueba.js"></script>
-				<h1 class="titulo-formulario">Nuevo Instrumento Notarial</h1>
+            <form>
+                    <h1 class="titulo-formulario">Nuevo Instrumento Notarial</h1>
 
                     <div class="campo-formulario">
                         <input id="ncliente" name="cliente" type="text" placeholder="Nombre de Cliente">
-						<input id="nnotaria" name="notaria" type="number" placeholder="Notaria">
-					</div>
+                        <input id="nnotaria" name="notaria" type="number" placeholder="Notaria">
+                    </div>
 
                     <div class="campo-formulario">
                         <input id="nuescritura" name="escritura" type="number" placeholder="N° de Escritura">
-						<input id="nciudad" name="ciudad" type="text" placeholder="Ciudad">
+                        <input id="nciudad" name="ciudad" type="text" placeholder="Ciudad">
                     </div>
 
                     <div class="campo-formulario">
                         <input id="ninstrumentacion" name="instrumentacion" type="text" placeholder="Instrumentación">
-						<select id="nubifisica" name="ubifisica">
-							<option value="0">Selecciona Ubicacion Fisica</option>
-							<option value="homero">Homero</option>
-							<option value="campos">Campos</option>
-							<option value="archivo">Archivo</option>
-						</select>
+                        <select id="nubifisica" name="ubifisica">
+                            <option value="0">Selecciona Ubicacion Fisica</option>
+                            <option value="homero">Homero</option>
+                            <option value="campos">Campos</option>
+                            <option value="archivo">Archivo</option>
+                        </select>
                     </div>
 
                     <div class="campo-formulario">
                         <select id="ntipdocum" name="tipdocum" onchange="mostrarCampoTestimonio()">
-							<option value="0">Tipo de Documento</option>
-							<option value="testimonio">Testimonio</option>
-							<option value="copia certificada">Copia Certificada</option>
-							<option value="copia simple">Copia Simple</option>
-						</select>
-                            <div class="campo-formulario" id="testimonioCampo" style="display: none;">
-                                <input id="numcertificado" name="ncertificado" type="number" placeholder="N° Certificado" oninput="concatenarTestimonio()">
-                                <p id="resultadoTestimonio"></p>
-                            </div>
-
-						<!--<input id="numcertificado" name="ncertificado" placeholder="N° Certificado">-->
-						<input id="nrutadrive" name="rutadrive" placeholder="Ruta en Drive">
+                            <option value="0">Tipo de Documento</option>
+                            <option value="testimonio">Testimonio</option>
+                            <option value="copia certificada">Copia Certificada</option>
+                            <option value="copia simple">Copia Simple</option>
+                        </select>
+                        <input id="nrutadrive" name="rutadrive" placeholder="Ruta en Drive">
                     </div>
 
-					<div class="campo-formulario">
-                        <input id="nejemplares" name="ejemplares" type="text" placeholder="Número de ejemplares">
-						<input id="ncodinter" name="codinter" type="text" placeholder="Codigo Interno" disabled>
-					
-					</div>
+                    <div class="campo-formulario" id="testimonioCampo" style="display: none;">
+                            <input id="numcertificado" name="ncertificado" type="number" placeholder="N° Certificado" oninput="concatenarTestimonio()">
+                            <p id="resultadoTestimonio"></p>
+                        </div>
 
+                    <div class="campo-formulario">
+                        <input id="nejemplares" name="ejemplares" type="text" placeholder="Número de ejemplares">
+                        <input id="ncodinter" name="codinter" type="text" placeholder="Codigo Interno" disabled>
+                    </div>
 
                     <div class="boton-contenedor-formulario">
-						<button type="button" id="btn-ncliente" class="boton-formulario" onclick="window.location.href='altaclientes.php'">Nuevo Cliente</button>
+                        <button type="button" id="btn-ncliente" class="boton-formulario" onclick="window.location.href='altaclientes.php'">Nuevo Cliente</button>
                         <button id="btn-registrar" class="boton-formulario">Registrar</button>
                     </div>
                 </form>
--- clases_prueba.js
+++ clases_prueba.js
@@ -83,3 +83,4 @@ function concatenarTestimonio() {
     resultado.textContent = `Testimonio ${numero}`;
 }
 
+
