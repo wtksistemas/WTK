4a24eb16ebdf0ebac99effa6d626fd8bc1e93115
wtksistemas <sistemas@whitakermexico.com>
2024-12-11 11:29:28 -0600

    error base de datos}

--- adm.whitakermexico.com/1/legal/php/dbconnect.php
+++ adm.whitakermexico.com/1/legal/php/dbconnect.php
@@ -6,22 +6,22 @@ require_once("config.php");
 
 $connn="hola ";
 $conn = mysqli_connect($host, $usr, $psr,$data_base)
-or die('Error: Database to host connection: '.mysqli_error());
+or die('Error: Database to host connection clientes: '.mysqli_error());
 
 $conn1 = mysqli_connect($host, $usr, $psr,$data_base)
-or die('Error: Database to host connection: '.mysqli_error());
+or die('Error: Database to host connection clientes: '.mysqli_error());
 
 $conn_arch = mysqli_connect($host, $usr, $psr,$data_base_archivo)
-or die('Error: Database to host connection: '.mysqli_error());
+or die('Error: Database to host connection archivo: '.mysqli_error());
 
 $conn1_arch = mysqli_connect($host, $usr, $psr,$data_base_archivo)
-or die('Error: Database to host connection: '.mysqli_error());
+or die('Error: Database to host connection archivo: '.mysqli_error());
 
 $conn_muni = mysqli_connect($host, $usr, $psr,$data_base_municipios)
-or die('Error: Database to host connection: '.mysqli_error());
+or die('Error: Database to host connection municipios: '.mysqli_error());
 
 $conn1_muni = mysqli_connect($host, $usr, $psr,$data_base_municipios)
-or die('Error: Database to host connection: '.mysqli_error());
+or die('Error: Database to host connection municipios: '.mysqli_error());
 	
 
 
