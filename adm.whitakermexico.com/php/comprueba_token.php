857ab4ecefedf4f05d24f9c373e5fad7d961e840
wtksistemas <167464828+wtksistemas@users.noreply.github.com>
2025-07-01 10:22:55 -0600

    Delete adm.whitakermexico.com directory

--- adm.whitakermexico.com/php/comprueba_token.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('funciones.php');
-$token_usr=$_POST['token'];
-$r=$_POST["url_token"];
-$resultado=com_tkn($token_usr,$r);
-?>
--- adm.whitakermexico.com/php/sesion.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once("funciones.php");
-$user = filter_input(INPUT_POST, 'username', FILTER_VALIDATE_EMAIL);
-$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
-login_user($user,$password);
-?>
